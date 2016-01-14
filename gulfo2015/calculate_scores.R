037d2266bd9691457778405365bb36dfdbeb0242
jules32 <jstewart@nceas.ucsb.edu>
2016-01-14 09:09:23 -0800

    intro w/ gustavo, create prep/AO/prep_social.r example

--- gulfo2015/calculate_scores.R
+++ gulfo2015/calculate_scores.R
@@ -1,5 +1,6 @@
 # load required libraries
-suppressWarnings(require(ohicore))
+library(tidyr)
+suppressWarnings(require(ohicore)) # source('install_ohicore.r')
 
 # set working directory to the scenario directory, ie containing conf and layers directories
 setwd('~/github/cdz/gulfo2015')
@@ -15,4 +16,5 @@ layers = Layers('layers.csv', 'layers')
 
 # calculate scenario scores
 scores = CalculateAll(conf, layers, debug=F)
+
 write.csv(scores, 'scores.csv', na='', row.names=F)
---
+++ gulfo2015/prep/AO/prep_social.r
@@ -0,0 +1,18 @@
+# preparing social data for X
+
+x = read.csv('')
+
+# formatting
+
+library(dplyr)
+library(tidyr)
+
+# rescaling
+
+# save to layers folder
+write.csv('/layers/ao_data_cdz2015')
+
+
+# register in layers.csv
+
+# modify goal model to use layer
